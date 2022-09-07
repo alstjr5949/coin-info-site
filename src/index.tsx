f0ddd5a67316939827d4338e9c0c298e726d3501
alstjr5949 <alstjr5949@gmail.com>
2022-09-07 15:28:45 +0900

    feat : recoil을 활용하기 위한 세팅 및 아톰 설정
    1. dark 모드 구현을 위한 전역상태관리로 recoil을 선택해 사용했습니다.
    2. atom을 설정했습니다.

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
-
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
-import "./font/font.css";
-
-export const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html {
-    font-size: 10px;
-    font-family: "GmarketSansMedium";
-  }
-  body {
-    background-color: #FCF6F5;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  button {
-    width: 100%;
-    background: none;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-  }
-  .ir {
-    position: absolute;
-    clip: rect(0,0,0,0);
-    width: 1px;
-    height: 1px;
-    margin: -1px;
-    overflow:hidden;
-  }
-  .hide {
-    display: none;
-  }
-`;
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient();
 
@@ -50,9 +10,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <>
-    <QueryClientProvider client={queryClient}>
-      <GlobalStyle />
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </>
 );
