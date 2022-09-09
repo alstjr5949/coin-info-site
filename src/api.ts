8ecbbd4349ae15287918bb067a6ef7c2f958a95c
alstjr5949 <alstjr5949@gmail.com>
2022-09-09 23:38:19 +0900

    feat : api url 변경 및 추가 api 함수 작성
    1. 코인파프리카에서 업비트 오픈 api로 변경했습니다.

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.coinpaprika.com/v1",
+  baseURL: "https://api.upbit.com/v1",
 });
 
 export const getAllCoin = async () => {
-  return api.get("/coins").then((res) => res.data.slice(0, 100));
+  return api.get("/market/all").then((res) => res.data);
+};
+
+export const getCoinTickerById = async (coinId: string) => {
+  return api.get(`/ticker?markets=${coinId}`).then((res) => res.data);
+};
+
+export const getCoinHistory = (coinId: string) => {
+  return api
+    .get(`/candles/weeks?market=${coinId}&count=20`)
+    .then((res) => res.data);
 };
