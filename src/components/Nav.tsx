fe57e3bf3b2d30f756027dc072a63a0e8cb40f48
alstjr5949 <alstjr5949@gmail.com>
2022-09-09 23:41:34 +0900

    design : 다크모드 토글 버튼 디자인

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 
 import CommonWrapper from "../common/commonWrapper";
 import Logo from "./Logo";
+import { useState } from "react";
 
 const Header = styled.header`
   display: flex;
@@ -17,6 +18,7 @@ const Header = styled.header`
 const NavWrapper = styled(CommonWrapper)`
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const StyledNav = styled.nav`
@@ -38,16 +40,42 @@ const NavItem = styled(Link)`
   font-size: 16px;
 `;
 
-const ThemeModeBtn = styled.button`
+const ThemeModeBtn = styled.button<{ buttonLeftPosition: string }>`
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => props.theme.nameColor};
   width: 40px;
+  height: 20px;
+  border-radius: 10px;
   color: ${(props) => props.theme.nameColor};
+  position: relative;
+  &::before {
+    content: "";
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    background-color: ${(props) => props.theme.boardColor};
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${(props) => props.buttonLeftPosition};
+    margin: auto 0;
+    transition: left 0.5s linear;
+  }
 `;
 
 const Nav = () => {
   const [isDark, setIsDark] = useRecoilState(isDarkAtom);
+  const [buttonLeftPosition, setButtonLeftPosition] = useState<string>("2px");
 
   const handleToggleBtnClick = () => {
     setIsDark((prev) => !prev);
+    if (buttonLeftPosition === "2px") {
+      setButtonLeftPosition("20px");
+    }
+    if (buttonLeftPosition === "20px") {
+      setButtonLeftPosition("2px");
+    }
   };
   return (
     <Header>
@@ -60,7 +88,10 @@ const Nav = () => {
             <NavItem to="/coin/KRW-BTC">Markets</NavItem>
           </NavList>
         </StyledNav>
-        <ThemeModeBtn onClick={handleToggleBtnClick}> 토글버튼</ThemeModeBtn>
+        <ThemeModeBtn
+          onClick={handleToggleBtnClick}
+          buttonLeftPosition={buttonLeftPosition}
+        ></ThemeModeBtn>
       </NavWrapper>
     </Header>
   );
