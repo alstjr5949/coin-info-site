adc78f20f4ac85e44f8a997829f97061f7912564
alstjr5949 <alstjr5949@gmail.com>
2022-09-12 23:19:50 +0900

    feat : ticker text color 설정
    1. RISE 이면 빨간색 FALL 이면 파란색 EVEN 이면 기본으로 설정했습니다.

--- src/components/CoinTicker.tsx
+++ src/components/CoinTicker.tsx
@@ -1,24 +1,27 @@
+import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { getCoinTickerById } from "../api";
 
-const CoinTickerPrice = styled.p`
+const CoinTickerPrice = styled.p<{ tickerColorProp: string }>`
   font-size: 24px;
   padding: 15px 20px;
+  color: ${(props) => props.tickerColorProp};
   span {
     font-size: 12px;
     margin-left: 5px;
   }
 `;
 
-const CoinChange = styled.p`
+const CoinChange = styled.p<{ tickerColorProp: string }>`
   display: flex;
   align-items: flex-start;
   gap: 10px;
   padding: 0px 20px 15px;
   border-bottom: 1px solid #b3b3b3;
   font-size: 14px;
+  color: ${(props) => props.tickerColorProp};
 `;
 
 const CoinChangeText = styled.span`
@@ -61,6 +64,7 @@ interface ICoinTicker {
 
 const CoinTicker = () => {
   const { coinId } = useParams();
+  const [tickerColor, setTickerColor] = useState<string>("#E9ECF1");
 
   const { isLoading: tickerLoading, data: tickerData } = useQuery<
     ICoinTicker[]
@@ -70,19 +74,31 @@ const CoinTicker = () => {
     refetchInterval: 1000,
   });
 
+  const tickerDataBool: string = tickerData ? tickerData[0].change : "";
+
+  useEffect(() => {
+    if (tickerDataBool === "RISE") {
+      setTickerColor("#D25044");
+    } else if (tickerDataBool === "FALL") {
+      setTickerColor("#1261C4");
+    } else if (tickerDataBool === "EVEN") {
+      setTickerColor("#E9ECF1");
+    }
+  }, [tickerData]);
+
   return (
     <>
       {tickerLoading ? (
         "Loading..."
       ) : (
         <>
-          <CoinTickerPrice>
+          <CoinTickerPrice tickerColorProp={`${tickerColor}`}>
             {tickerData
               ? tickerData[0].trade_price.toLocaleString("ko-KR")
               : ""}
             <span>KRW</span>
           </CoinTickerPrice>
-          <CoinChange>
+          <CoinChange tickerColorProp={`${tickerColor}`}>
             <CoinChangeText>전일대비</CoinChangeText>
             <CoinChangeRate>
               {tickerData
